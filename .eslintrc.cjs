2f23b9f81cb6a07c9490da19a2930fdb165dfa66
רוט <c0527119724@gmail.com>
2024-03-04 21:23:24 +0200

    some commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,6 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-<<<<<<< HEAD
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
@@ -14,15 +13,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-=======
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
->>>>>>> oof
+
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
